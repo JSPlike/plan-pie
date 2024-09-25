af2d9929b344e9044d79a1c885504e60d2e740ba
JSPlike <moter2588@gmail.com>
2024-09-25 13:30:24 +0900

    springboot initialized

---
+++ src/main/java/hanco/planpie/PlanpieApplication.java
@@ -0,0 +1,13 @@
+package hanco.planpie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanpieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanpieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanco/planpie/PlanpieApplicationTests.java
@@ -0,0 +1,13 @@
+package hanco.planpie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanpieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
