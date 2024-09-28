0c9c09481711f6fde00fc819df0d2aa8b86ff2be
jcoding <moter2588@gmail.com>
2024-09-28 09:35:07 +0900

    home 화면 정리

--- src/main/resources/static/biz/dashboard/kanbanboard.js
+++ src/main/resources/static/biz/dashboard/kanbanboard.js
@@ -16,6 +16,9 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault(); // 기본 동작 방지
     var data = ev.dataTransfer.getData("text"); // 드래그한 요소의 ID 가져오기
+    console.log(">>>>>>>>요소 드래그");
+    console.log(data);
+
     // 드롭한 위치가 칸반 열인지 확인
     if (ev.target.classList.contains("kanban-column")) {
         ev.target.appendChild(document.getElementById(data)); // 드롭한 위치에 요소 추가
